e987d54c158b0eff570876a00f4ba54a43f3d1b2
Ishay Yemini <ishay.yemini@gmail.com>
2025-05-07 17:54:34 +0300

    Fixed in game_manager_test.cpp

--- hw_1/tests/game_manager_test.cpp
+++ hw_1/tests/game_manager_test.cpp
@@ -14,7 +14,7 @@ public:
     MockAlgorithm(Action defaultAction) : defaultAction(defaultAction) {
     }
 
-    Action decideAction(const GameState &_) override {
+    Action decideAction(const GameState &) override {
         return defaultAction;
     }
 
