1326bc7591e052e156948957e30b06d4da4a784b
Ishay Yemini <ishay.yemini@gmail.com>
2025-06-07 22:48:39 +0300

    Different players

--- hw_2/src/algo/Player1.cpp
+++ hw_2/src/algo/Player1.cpp
@@ -1,12 +1,13 @@
-#include "MyActualPlayer.h"
+#include "Player1.h"
+
 #include "MyBattleInfo.h"
 
-void MyActualPlayer::updateTankWithBattleInfo(TankAlgorithm &tank, SatelliteView &satellite_view) {
+void Player1::updateTankWithBattleInfo(TankAlgorithm &tank, SatelliteView &satellite_view) {
     auto battle_info = MyBattleInfo(createBoardFromSatellite(satellite_view), player_index, max_steps, num_shells);
     tank.updateBattleInfo(battle_info);
 }
 
-std::vector<std::vector<char> > MyActualPlayer::createBoardFromSatellite(const SatelliteView &satellite_view) const {
+std::vector<std::vector<char> > Player1::createBoardFromSatellite(const SatelliteView &satellite_view) const {
     std::vector board(x, std::vector<char>(y));
     for (size_t i{0}; i < x; i++) {
         for (size_t j{0}; j < y; j++) {
