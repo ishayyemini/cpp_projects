cad1fc7a79cae281e8b0f2d13d381fcd4d016a47
Ishay Yemini <ishay.yemini@gmail.com>
2025-06-07 18:22:10 +0300

    Should be like defined

--- hw_2/common/Player.h
+++ hw_2/common/Player.h
@@ -6,17 +6,13 @@
 
 class Player {
 public:
-    Player(int, size_t, size_t, size_t, size_t){}
+    Player(int, size_t, size_t, size_t, size_t) {
+    }
 
     virtual ~Player() = default;
 
-    Player(const Player&) = delete;
-    Player& operator=(const Player&) = delete;
-    Player(Player&&) = delete;
-    Player& operator=(Player&&) = delete;
-
     virtual void updateTankWithBattleInfo(
-            TankAlgorithm &tank, SatelliteView &satellite_view) = 0;
+        TankAlgorithm &tank, SatelliteView &satellite_view) = 0;
 };
 
 #endif // PLAYER_H
