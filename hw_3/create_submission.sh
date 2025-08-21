5c9f1784e687f304d26604cb3256d639e16216af
Ishay Yemini <ishay.yemini@gmail.com>
2025-08-21 19:00:12 +0300

    Added submission

---
+++ hw_3/create_submission.sh
@@ -0,0 +1,23 @@
+ID=322868852_340849710
+
+make clean
+
+mkdir $ID
+
+cp -r Algorithm $ID/Algorithm
+cp -r common $ID/common
+cp -r GameManager $ID/GameManager
+cp -r Simulator $ID/Simulator
+cp -r UserCommon $ID/UserCommon
+
+rm $ID/Algorithm/CMakeLists.txt
+rm $ID/GameManager/CMakeLists.txt
+rm $ID/Simulator/CMakeLists.txt
+
+cp Makefile $ID/Makefile
+cp README.md $ID/README.md
+cp students.txt $ID/students.txt
+
+zip -r $ID.zip $ID
+
+rm -r $ID
